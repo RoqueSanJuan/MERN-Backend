2954572fa7d45eb21a0c3bf53ba50a36e8c91764
roque <roque.m.sanjuan@gmail.com>
2021-01-27 01:53:58 -0300

    ajustes para deploy

--- index.js
+++ index.js
@@ -26,11 +26,13 @@ const port = process.env.port || 4000;
 //Importar rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
 app.use('/api/auth', require('./routes/auth'));
-app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas'));
+app.use('/api/proyectos', require('./routes/proyectos'));
+
+//Agrego cambios
 
 
 //Arranca la app
-app.listen(port, '0.0.0.0', () => {
+app.listen(port,'0.0.0.0', () => {
     console.log(`El servidor esta funcionando en el puerto ${port}`);
 })
